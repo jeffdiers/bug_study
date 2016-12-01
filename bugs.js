95ad1efa66680965baf4b340bd932eb511450513
Nmuta Jones <nmuta1@gmail.com>
2016-12-01 13:11:52 -0700

    bugz2

--- bugs.js
+++ bugs.js
@@ -1,17 +1,17 @@
 //dynamically get the year and add to the footer
 
 $(document).ready(function(){
-  
+
   $("#year").html(current_year);
   var current_year = new Date().getFullYear();
 
   var bug_list = ["butterfly", "ladybug", "mosquito", "pincher", "mantis"];
 
-  var random_bug_number = Math.floor(Math.random() * bug_list.length + 1;
+  var random_bug_number = Math.floor(Math.random() * bug_list.length + 1);
   $("#author").html(bug_list[random_bug_number]);
 
   for (var i = 0; i < bug_list.length; i++) {
-    $("#bug_display").append("<li>"+bug_list[i]+"</li>");
+    $("#bug_display").append("<li>"+bug_list[i]+"<img src='images/"+bug_list[i]+".jpg'/></li>");
   }
 
 })
